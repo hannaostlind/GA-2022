ae08362f9b90d16c6843fff70bd91eea069192e8
AlbaMoriWallin123 <94969096+AlbaMoriWallin123@users.noreply.github.com>
2022-10-09 22:06:50 +0200

    Större grund

--- app.rb
+++ app.rb
@@ -4,11 +4,27 @@ require 'sinatra/reloader'
 
 
 get('/hem') do
+    @platser[
+    "Aleppo", 
+    "Burger King", 
+    "Baguetterian", 
+    "Cafét", 
+    "Coop", 
+    "Golden Lee", 
+    "Jättebaguetten", 
+    "Le pain", 
+    "Pita", 
+    "Seaside", 
+    "Subway; Domkyrkan", "Subway; Kungsportsplatsen", "Subway; Olivedal", 
+    "Sushi Vimi", 
+    "Taj Mahal", 
+    "Picaldei", 
+    "7-eleven; Nordstan", "7-eleven; Göteborg Central"]
     slim(:hem)
 end
 
 get('/hjul') do
-
+    slim(:hjul)
 end
 
 get('/tabell') do
